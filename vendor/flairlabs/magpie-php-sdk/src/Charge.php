32a9fa69c9385878092d2badb1027149d01f755a
varietyshow <varietyshow.booking@gmail.com>
2025-04-20 05:44:26 -0700

    Add files via upload

---
+++ vendor/flairlabs/magpie-php-sdk/src/Charge.php
@@ -0,0 +1,161 @@
+<?php
+
+namespace MagpieApi;
+
+/**
+ * This class holds the APIs anything related to charge.
+ */
+class Charge
+{
+    const URI = '/charges';
+    private $baseUrl;
+    private $isSandbox;
+    private $request;
+    private $key;
+
+    /**
+     * Class constructor.
+     *
+     * @param bool   $isSandbox Determines if the API will use sandbox URL or production URL
+     * @param string $key       The authorization key to be used for calling endpoints
+     */
+    public function __construct($isSandbox = false, $key = null, $version)
+    {
+        $this->isSandbox = $isSandbox;
+        $this->request = new Request($this->isSandbox);
+        $this->key = $key;
+        $this->version = $version;
+    }
+
+    public function __set($name, $value)
+    {
+        switch($name) {
+            case 'key':
+                $this->key = $value;
+                break;
+            case 'isSandbox':
+                $this->isSandbox = $value;
+                $this->request->isSandbox = $this->isSandbox;
+                break;
+            case 'version':
+                $this->version = $value;
+        }
+    }
+
+    /**
+     * Creates a charge.
+     *
+     * @param float  $amount              Amount of the charge. Should be: <the expected amount> times 100
+     * @param string $currency            Currency of the charge
+     * @param string $source              Source of the charge. Should be a token id
+     * @param string $description         Description of the charge
+     * @param string $statementDescriptor Statement descriptor of the charge
+     * @param bool   $capture             Determines if the charge should capture or not. true or false
+     *
+     * @return Magpie\Response Response from the API
+     */
+    public function create(
+        $amount,
+        $currency,
+        $source,
+        $description,
+        $statementDescriptor,
+        $capture
+    ) {
+        $response = $this->request->post(
+            '/' . $this->version . self::URI,
+            $this->key,
+            array(
+                'amount' => $amount,
+                'currency' => $currency,
+                'source' => $source,
+                'description' => $description,
+                'statement_descriptor' => $statementDescriptor,
+                'capture' => $capture,
+            )
+        );
+        $response->successCode(201);
+
+        return $response;
+    }
+
+    /**
+     * Gets a specific charge.
+     *
+     * @param string $id Charge ID to fetch
+     *
+     * @return Magpie\Response Response from the API
+     */
+    public function get($id)
+    {
+        $response = $this->request->get(
+            '/' . $this->version . self::URI.'/'.$id,
+            $this->key
+        );
+        $response->successCode(200);
+
+        return $response;
+    }
+
+    /**
+     * Capture a charge.
+     *
+     * @param string $id     Charge ID to capture
+     * @param float  $amount Amount to capture from $id charge id. Should be: <expected amount> times 100
+     *
+     * @return Magpie\Response Response from the API
+     */
+    public function capture($id, $amount)
+    {
+        $response = $this->request->post(
+            '/' . $this->version . self::URI.'/'.$id.'/capture',
+            $this->key,
+            array(
+                'amount' => $amount,
+            )
+        );
+        $response->successCode(200);
+
+        return $response;
+    }
+
+    /**
+     * Void a charge.
+     *
+     * @param string $id Charge ID to void
+     *
+     * @return Magpie\Response Response from the API
+     */
+    public function void($id)
+    {
+        $response = $this->request->post(
+            '/' . $this->version . self::URI.'/'.$id.'/void',
+            $this->key
+        );
+        $response->successCode(200);
+
+        return $response;
+    }
+
+    /**
+     * Refunds a charge.
+     *
+     * @param string $id     Charge ID to refund
+     * @param float  $amount Amount to refund from a charge. Should be: <expected amount> times 100
+     *
+     * @return Magpie\Response Response of the API
+     */
+    public function refund($id, $amount)
+    {
+        $response = $this->request->post(
+            '/' . $this->version . self::URI.'/'.$id.'/refund',
+            $this->key,
+            array(
+                'amount' => $amount,
+            )
+        );
+        $response->successCode(200);
+
+        return $response;
+    }
+}
---
+++ vendor/flairlabs/magpie-php-sdk/src/Magpie.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace MagpieApi;
+
+class Magpie
+{
+    private $isSandbox = false;
+    private $currentUrl = '';
+    private $pkKey = '';
+    private $skKey = '';
+    private $curl;
+    private $token;
+    private $charge;
+    private $customer;
+    private $version;
+
+    public function __construct($pkKey = null, $skKey = null, $isSandbox = false, $version = 'v1')
+    {
+        if (!in_array($version, ['v1', 'v1.1'])) {
+            throw new \Exception('Invalid version. Choose either v1 or v1.1');
+        }
+
+        $this->pkKey = $pkKey;
+        $this->skKey = $skKey;
+        $this->isSandbox = $isSandbox;
+        $this->version = $version;
+        $this->token = new Token($this->isSandbox, $this->pkKey);
+        $this->charge = new Charge($this->isSandbox, $this->skKey, $this->version);
+        $this->customer = new Customer($this->isSandbox, $this->skKey);
+    }
+
+    public function __set($name, $value) {
+        switch($name)
+        {
+            case 'pkKey':
+                $this->pkKey = $value;
+                $this->token->key = $value;
+                break;
+            case 'skKey':
+                $this->skKey = $value;
+                $this->charge->key = $value;
+                $this->customer->key = $value;
+                break;
+            case 'isSandbox':
+                $this->isSandbox = $value;
+                $this->token->isSandbox = $value;
+                $this->charge->isSandbox = $value;
+                $this->customer->isSandbox = $value;
+                break;
+            case 'version':
+                if (!in_array($value, ['v1', 'v1.1'])) {
+                    throw new \Exception('Invalid version. Choose either v1 or v1.1');
+                }
+
+                $this->version = $value;
+                $this->charge->version = $this->value;
+        }
+    }
+
+    public function __get($name) {
+        if ($name === 'token') {
+            return $this->token;
+        }
+        switch($name) {
+            case 'token':
+                return $this->token;
+                break;
+            case 'charge':
+                return $this->charge;
+                break;
+            case 'customer':
+                return $this->customer;
+                break;
+        }
+    }
+}
