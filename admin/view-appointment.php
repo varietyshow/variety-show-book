e85edd026c71473e267060cdf6940a4b4eb1967c
varietyshow <varietyshow.booking@gmail.com>
2025-04-25 06:58:39 +0800

    Update view-appointment.php

--- admin/view-appointment.php
+++ admin/view-appointment.php
@@ -21,10 +21,10 @@ if (!$book_id) {
 }
 
 // Database connection
-$host = 'sql12.freesqldatabase.com';
-$dbname = 'sql12774230';
-$username = 'sql12774230';
-$password = 'ytPEFx33BF';
+$host = 'localhost';
+$dbname = 'db_booking_system';
+$username = 'root';
+$password = '';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
