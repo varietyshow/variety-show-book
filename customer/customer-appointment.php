cec6f0b165c64a8ded08d64eb21e1553e5a21c27
varietyshow <varietyshow.booking@gmail.com>
2025-04-28 06:54:44 +0800

    Update customer-appointment.php

--- customer/customer-appointment.php
+++ customer/customer-appointment.php
@@ -11,9 +11,9 @@ $first_name = htmlspecialchars($_SESSION['first_name']); // Retrieve and sanitiz
 
 // Database connection
 $servername = "sql12.freesqldatabase.com"; // Update with your server name
-$username = "sql12774230"; // Update with your database username
-$password = "ytPEFx33BF"; // Update with your database password
-$dbname = "sql12774230"; // Update with your database name
+$username = "sql12775634"; // Update with your database username
+$password = "kPZFb8pXsU"; // Update with your database password
+$dbname = "sql12775634"; // Update with your database name
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
