b2045661eb992f2eea67141903703c583e2b34f9
varietyshow <varietyshow.booking@gmail.com>
2025-04-28 06:53:10 +0800

    Update check-entertainer-availability.php

--- customer/check-entertainer-availability.php
+++ customer/check-entertainer-availability.php
@@ -13,10 +13,10 @@ function sendJsonResponse($success, $available, $error = '') {
 }
 
 // Database connection
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "db_booking_system";
+$servername = "sql12.freesqldatabase.com";
+$username = "sql12775634";
+$password = "kPZFb8pXsU";
+$dbname = "sql12775634";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
