c18dfff27266b8e169d329d406a306ac76d0e747
varietyshow <varietyshow.booking@gmail.com>
2025-04-28 06:59:20 +0800

    Update process-reschedule.php

--- customer/process-reschedule.php
+++ customer/process-reschedule.php
@@ -12,10 +12,10 @@ $new_time_start = $_POST['new_time_start'];
 $new_time_end = $_POST['new_time_end'];
 
 // Database connection
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "db_booking_system";
+$servername = "sql12.freesqldatabase.com";
+$username = "sql12775634";
+$password = "kPZFb8pXsU";
+$dbname = "sql12775634";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
