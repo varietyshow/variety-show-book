09d6f01b00d6ca53e5e0a017d599c8d601960673
varietyshow <varietyshow.booking@gmail.com>
2025-05-08 11:29:41 +0800

    Update admin-dashboard.php

--- admin/admin-dashboard.php
+++ admin/admin-dashboard.php
@@ -11,9 +11,9 @@ $first_name = htmlspecialchars($_SESSION['first_name']);
 
 // Database connection
 $host = 'sql12.freesqldatabase.com';
-$dbname = 'sql12775634';
-$username = 'sql12775634';
-$password = 'kPZFb8pXsU';
+$dbname = 'sql12777569';
+$username = 'sql12777569';
+$password = 'QlgHSeuU1n';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
