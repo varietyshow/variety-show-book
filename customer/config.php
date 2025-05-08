f3c6fcf71b78c1aafc3942ebdbcf66e0eacafb92
varietyshow <varietyshow.booking@gmail.com>
2025-05-08 11:48:07 +0800

    Update config.php

--- customer/config.php
+++ customer/config.php
@@ -2,8 +2,8 @@
 // config.php
 return [
     'db_host' => getenv('DB_HOST') ?: 'sql12.freesqldatabase.com',
-    'db_user' => getenv('DB_USER') ?: 'sql12775634',
-    'db_pass' => getenv('DB_PASS') ?: 'kPZFb8pXsU',
-    'db_name' => getenv('DB_NAME') ?: 'sql12775634'
+    'db_user' => getenv('DB_USER') ?: 'sql12777569',
+    'db_pass' => getenv('DB_PASS') ?: 'QlgHSeuU1n',
+    'db_name' => getenv('DB_NAME') ?: 'sql12777569'
 ];
 ?>
