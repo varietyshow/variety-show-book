81a556805622a903fb639f31c371cd004e7f4115
varietyshow <varietyshow.booking@gmail.com>
2025-05-08 12:03:24 +0800

    Update scheduleAppointment.php

--- entertainer/scheduleAppointment.php
+++ entertainer/scheduleAppointment.php
@@ -33,9 +33,9 @@ try {
 
     // Database connection
     $servername = "sql12.freesqldatabase.com";
-    $username = "sql12775634";
-    $password = "kPZFb8pXsU";
-    $dbname = "sql12775634";
+    $username = "sql12777569";
+    $password = "QlgHSeuU1n";
+    $dbname = "sql12777569";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
     
