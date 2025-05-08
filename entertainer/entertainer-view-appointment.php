775756c4da31b2d5c4e159a381f2a78b7e7d5361
varietyshow <varietyshow.booking@gmail.com>
2025-05-08 12:00:19 +0800

    Update entertainer-view-appointment.php

--- entertainer/entertainer-view-appointment.php
+++ entertainer/entertainer-view-appointment.php
@@ -22,9 +22,9 @@ if (!$book_id) {
 
 // Database connection
 $host = 'sql12.freesqldatabase.com';
-$dbname = 'sql12775634';
-$username = 'sql12775634';
-$password = 'kPZFb8pXsU';
+$dbname = 'sql12777569';
+$username = 'sql12777569';
+$password = 'QlgHSeuU1n';
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
