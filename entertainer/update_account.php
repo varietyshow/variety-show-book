140642d555e8dd5240b54d20f500786b4740948b
varietyshow <varietyshow.booking@gmail.com>
2025-05-08 12:04:14 +0800

    Update update_account.php

--- entertainer/update_account.php
+++ entertainer/update_account.php
@@ -6,9 +6,9 @@ if (!isset($_SESSION['first_name'])) {
 }
 
 $host = 'sql12.freesqldatabase.com';
-$dbname = 'sql12775634';
-$username = 'sql12775634';
-$password = 'kPZFb8pXsU';
+$dbname = 'sql12777569';
+$username = 'sql12777569';
+$password = 'QlgHSeuU1n';
 
 $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) {
