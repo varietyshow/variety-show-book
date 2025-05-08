e085d7e6fb026c2dc7b7b18171bfbe809a5979e9
varietyshow <varietyshow.booking@gmail.com>
2025-05-08 11:38:00 +0800

    Update get-package-details.php

--- admin/get-package-details.php
+++ admin/get-package-details.php
@@ -9,9 +9,9 @@ if (!isset($_SESSION['first_name'])) {
 
 // Database connection
 $servername = "sql12.freesqldatabase.com";
-$username = "sql12775634";
-$password = "kPZFb8pXsU";
-$dbname = "sql12775634";
+$username = "sql12777569";
+$password = "QlgHSeuU1n";
+$dbname = "sql12777569";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
