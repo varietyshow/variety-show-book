bc54fb4b1ec7384a084d28fbab0bd7fab67c5de5
varietyshow <varietyshow.booking@gmail.com>
2025-05-08 12:03:50 +0800

    Update update-password.php

--- entertainer/update-password.php
+++ entertainer/update-password.php
@@ -8,9 +8,9 @@ if (!isset($_SESSION['first_name'])) {
 
 // Database configuration
 $host = 'sql12.freesqldatabase.com';
-$dbname = 'sql12775634';
-$username = 'sql12775634';
-$password = 'kPZFb8pXsU';
+$dbname = 'sql12777569';
+$username = 'sql12777569';
+$password = 'QlgHSeuU1n';
 
 try {
     $conn = new mysqli($host, $username, $password, $dbname);
